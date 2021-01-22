beada21bf6227e14ad1f86b8b8019489d90875fe
toutevukantabu <toutevukantabu@users.noreply.github.com>
2021-01-22 20:46:26 +0100

    Premieres pages

---
+++ src/Controller/TestController.php
@@ -0,0 +1,30 @@
+<?php 
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+    public function index(){
+    
+      dd("ça fonctionne");
+  
+    }
+
+  /**
+  * @Route("/test/{age</d+>?0"} , name = "test", methods={"GET", "POST"},
+  *  host = "localhost , schemes={"http" , "https" })
+  */
+    public function test(Request $request , $age){
+
+
+ return new Response("vous avez $age ans !");
+    }
+}
+
+
+
+
+ ?>
