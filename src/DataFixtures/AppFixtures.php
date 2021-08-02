b4707cd2e415b7abdca742115d2745fcd22a6b76
toutvukantabu <gwendal.bescont@gmail.com>
2021-08-03 01:09:52 +0200

    feat : user auth

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,49 +2,71 @@
 
 namespace App\DataFixtures;
 
-use WW\Faker\Provider\Picture;
-use App\Entity\Product;
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
-use Symfony\Component\String\Slugger\SluggerInterface;
+use App\Entity\User;
+use App\Entity\Product;
 use App\Entity\Category;
+use WW\Faker\Provider\Picture;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 
 {
     protected $slugger;
+    protected $encoder;
 
-    public function __construct(SluggerInterface $slugger)
+    public function __construct(SluggerInterface $slugger, UserPasswordEncoderInterface $encoder)
     {
         $this->slugger = $slugger;
+        $this->encoder = $encoder;
     }
     public function load(ObjectManager $manager)
     {
+
         $faker = Factory::create('fr_FR');
-        $faker-> addProvider(new \Liior\Faker\Prices($faker));
-        $faker-> addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
-      $faker->addProvider(new \WW\Faker\Provider\Picture($faker));
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+        $faker->addProvider(new \WW\Faker\Provider\Picture($faker));
 
-        for($c=0 ; $c<3; $c++){
-$category = new Category;
-$category->setName($faker->department)
-->setSlug(strtolower($this->slugger->slug($category->getName())));
+        $admin = new User;
+        $hash = $this->encoder->encodePassword($admin, "password");
+        $admin->setFullName($faker->name())
+            ->setEmail("admin@gmail.com")
+            ->setPassword($hash)
+            ->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
 
-$manager->persist($category);
 
-for ($p = 0; $p < mt_rand(15 , 20); $p++) {
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User();
+            $hash = $this->encoder->encodePassword($user, "password");
+            $user->setFullName($faker->name())
+                ->setEmail("$u@gmail.com")
+                ->setPassword($hash);
+            $manager->persist($user);
+        }
+        for ($c = 0; $c < 3; $c++) {
+            $category = new Category;
+            $category->setName($faker->department)
+                ->setSlug(strtolower($this->slugger->slug($category->getName())));
 
-    $product = new Product;
-    $product->setName($faker->productName)
-        ->setPrice($faker->price(4000, 20000))
-        ->setSlug(strtolower($this->slugger->slug($product->getName())))
-        ->setCategory($category)
-        ->setShortDescription($faker->paragraph())
-        ->setMainPicture($faker->pictureUrl(250,200));
+            $manager->persist($category);
 
-    $manager->persist($product);
-}
+            for ($p = 0; $p < mt_rand(15, 20); $p++) {
+
+                $product = new Product;
+                $product->setName($faker->productName)
+                    ->setPrice($faker->price(4000, 20000))
+                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    ->setCategory($category)
+                    ->setShortDescription($faker->paragraph())
+                    ->setMainPicture($faker->pictureUrl(250, 200));
+
+                $manager->persist($product);
+            }
         }
         $manager->flush();
     }
